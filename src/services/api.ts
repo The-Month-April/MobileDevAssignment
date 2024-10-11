5808922b7f8d0845c0b362aea3946394fa7d8ea7
AMAllen15 <allena294@gmail.com>
2024-10-10 23:37:06 -0600

    UI Change to Login Page as well as npm install

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,23 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from "axios";
+import Config from "react-native-config";
 
 const api = axios.create({
-    // Before running your 'json-server', get your computer's IP address and
-    // update your baseURL to `http://your_ip_address_here:3333` and then run:
-    // `npx json-server --watch db.json --port 3333 --host your_ip_address_here`
-    //
-    // To access your server online without running json-server locally,
-    // you can set your baseURL to:
-    // `https://my-json-server.typicode.com/<your-github-username>/<your-github-repo>`
-    //
-    // To use `my-json-server`, make sure your `db.json` is located at the repo root.
+  // Before running your 'json-server', get your computer's IP address and
+  // update your baseURL to `http://your_ip_address_here:3333` and then run:
+  // `npx json-server --watch db.json --port 3333 --host your_ip_address_here`
+  //
+  // To access your server online without running json-server locally,
+  // you can set your baseURL to:
+  // `https://my-json-server.typicode.com/<your-github-username>/<your-github-repo>`
+  //
+  // To use `my-json-server`, make sure your `db.json` is located at the repo root.
 
-    baseURL: 'http://0.0.0.0:3333',
+  baseURL: Config.MY_IP_ADDRESS,
 });
 
-export const authenticateUser = (email: string, password: string): Promise<AxiosResponse> => {
-    return api.post(`/login`, { email, password });
+export const authenticateUser = (
+  email: string,
+  password: string
+): Promise<AxiosResponse> => {
+  return api.post(`/login`, { email, password });
 };
